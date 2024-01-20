00efee9c532cb1d566e4e8c2ff1472f7da101323
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-01-20 17:16:06 +0800

    second commit

--- exercise_1.py
+++ exercise_1.py
@@ -3,11 +3,23 @@ number1 = 20
 number2 = 30
 
 finaloutcome = number1 * number2
+
+if finaloutcome <= 1000:
+    finaloutcome = finaloutcome
+else:
+    finaloutcome = number1 + number2
+
 print("The result is", finaloutcome)
 
 # Given 2
 number1 = 40
 number2 = 30
 
-finaloutcome = number1 + number2
+finaloutcome = number1 * number2
+
+if finaloutcome <= 1000:
+    finaloutcome = finaloutcome
+else:
+    finaloutcome = number1 + number2
+
 print("The result is", finaloutcome)
