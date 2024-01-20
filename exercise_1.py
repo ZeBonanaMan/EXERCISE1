3c04a249bccd893e24e6f1c37c93df1d369dc39d
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-01-20 16:58:44 +0800

    first commit

---
+++ exercise_1.py
@@ -0,0 +1,13 @@
+# Given 1
+number1 = 20
+number2 = 30
+
+finaloutcome = number1 * number2
+print("The result is", finaloutcome)
+
+# Given 2
+number1 = 40
+number2 = 30
+
+finaloutcome = number1 + number2
+print("The result is", finaloutcome)
